c9bf6bfcce3dee94cb0cb2a4923d7c4928aaf469
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-08-07 15:25:06 +0530

    crate vite app using typescript & config tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-2xl font-sans text-pink-700 font-bold">
+        In app components!
+      </p>
+    </>
+  );
+}
+
+export default App;
