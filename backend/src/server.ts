6a8ddf2275bccefaa9e2cc6aa8fc844add8ccb80
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-08-07 15:17:24 +0530

    create express app & connect to MongoDB

---
+++ backend/src/server.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import app from "./app";
+import connectDB from "./database/connectDB";
+
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION!! Shuting down...");
+  console.log(err);
+  process.exit(1);
+});
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    const server = app.listen(PORT, () => {
+      console.log(`Server is running on PORT: ${PORT}`);
+    });
+
+    process.on("unhandledRejection", (err) => {
+      console.error("Unhandled Promise rejection ", err);
+      server.close(() => {
+        process.exit(1);
+      });
+    });
+  })
+  .catch((error) => {
+    console.error("Error!!! While connecting to MongoDB", error);
+    process.exit(1);
+  });
