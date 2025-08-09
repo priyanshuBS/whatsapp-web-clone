6ff6c0e2c5860dd7801e20ae8295e69b66ad1650
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-08-09 17:21:06 +0530

    initilize frontend and install tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen bg-white dark:bg-gray-900">
+      <p className="text-2xl text-pink-700">In app components!</p>
+    </div>
+  );
+};
+
+export default App;
